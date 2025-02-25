60600010bf44415c4f89e478c79a831acac7edf7
Holices Junior <dev.holices@gmail.com>
2025-02-25 23:57:21 +0000

    feat: Create functions

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -2,10 +2,8 @@ import type { Config } from 'drizzle-kit'
 import { env } from './src/env'
 
 export default {
-  schema: './src/drizzle/schema/*',
-  out: './src/drizzle/migrations',
+  schema: 'src/drizzle/schema/*',
+  out: 'src/drizzle/migrations',
   dialect: 'postgresql',
-  dbCredentials: {
-    url: env.POSTGRES_URL,
-  },
+  dbCredentials: { url: env.DATABASE_URL },
 } satisfies Config
