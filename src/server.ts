da1f3e21a4349c82b24e512145e65e3b2bd1f12c
Holices Junior <dev.holices@gmail.com>
2025-02-26 00:06:54 +0000

    chore: Use env port and remove docker entrypoint

--- src/server.ts
+++ src/server.ts
@@ -1,18 +1,21 @@
-import fastifyCors from '@fastify/cors'
+import { fastifyCors } from '@fastify/cors'
 import { fastifySwagger } from '@fastify/swagger'
 import { fastifySwaggerUi } from '@fastify/swagger-ui'
-import fastify from 'fastify'
+import { fastify } from 'fastify'
 import {
-  type ZodTypeProvider,
   jsonSchemaTransform,
   serializerCompiler,
   validatorCompiler,
 } from 'fastify-type-provider-zod'
 import { env } from './env'
-import { accessInviteLinkRoute } from './routes/access-invite-link'
+import { accessInviteLinkRoute } from './routes/access-invite-link-route'
+import { getRankingRoute } from './routes/get-ranking-route'
+import { getSubscriberInvitesClicksRoute } from './routes/get-subscriber-invites-clicks-route'
+import { getSubscriberInvitesCountRoute } from './routes/get-subscriber-invites-count-route'
+import { getSubscriberRankingPositionRoute } from './routes/get-subscriber-ranking-position'
 import { subscribeToEventRoute } from './routes/subscribe-to-event-route'
 
-const app = fastify().withTypeProvider<ZodTypeProvider>()
+const app = fastify()
 
 app.setSerializerCompiler(serializerCompiler)
 app.setValidatorCompiler(validatorCompiler)
@@ -22,8 +25,8 @@ app.register(fastifyCors)
 app.register(fastifySwagger, {
   openapi: {
     info: {
-      title: 'Rankify',
-      version: '0.0.1',
+      title: 'Rankinfy',
+      version: '0.1',
     },
   },
   transform: jsonSchemaTransform,
@@ -35,11 +38,11 @@ app.register(fastifySwaggerUi, {
 
 app.register(subscribeToEventRoute)
 app.register(accessInviteLinkRoute)
+app.register(getRankingRoute)
+app.register(getSubscriberInvitesCountRoute)
+app.register(getSubscriberInvitesClicksRoute)
+app.register(getSubscriberRankingPositionRoute)
 
-app
-  .listen({ port: env.PORT })
-  .then(() => console.log(`Server is running on port ${env.PORT}`))
-  .catch(err => {
-    console.error('Error starting server', err)
-    process.exit(1)
-  })
+app.listen({ port: env.PORT }).then(() => {
+  console.log('HTTP server running!')
+})
